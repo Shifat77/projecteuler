8afaa00571d1ecfb5e2deba6adcae1aa9675eb89
Shifat77 <85871622+Shifat77@users.noreply.github.com>
2023-03-19 10:36:30 +0600

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,15 +1,25 @@
 #more efficirnt code requre
-input = 600851475143
-for number in range (2, input):
-  if input%number == 0:
-    def prime(number):
-      def primeCheck (number):
-        for x in range(2, number):
-          y =  number%x
-      
+number = 600851475143
+x = 1
+
+for x in range(1, number):
+
+  x -= number
+  x = abs(x)
+
+  if number % x == 0:
+
+    def prime(x):
+
+      def primeCheck(x):
+        for k in range(2, x):
+          y = x % k
+
           if y == 0:
             return False
-      if primeCheck (number) != False:
+
+      if primeCheck(x) != False:
         return True
-    if prime(number) == True:
-      print(number)
+
+    if prime(x) == True:
+      print(x)
