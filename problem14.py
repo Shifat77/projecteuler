6838dcea36cf06cf599d24d60c3ca8e7dc4a2059
Shifat77 <85871622+Shifat77@users.noreply.github.com>
2023-03-19 22:44:34 +0600

    Update problem14.py

--- problem14.py
+++ problem14.py
@@ -1,8 +1,9 @@
-#vul ache
+numOfElementOfSeries =1
 for b in range(1,1000001):
   highest =1
   i = 1
   number = b
+  
   while number !=1 :
     if number%2 ==0:
       seriesNum = number/2
@@ -10,14 +11,11 @@ for b in range(1,1000001):
       seriesNum = number *3 +1
     number = seriesNum
     i+= 1
-  if highest <i :
+    
+  if numOfElementOfSeries <i :
     highest = i
     thatNum = b
     numOfElementOfSeries = highest
-    
-  # print(i)
-
-
-
+  
 print(thatNum)
 print(numOfElementOfSeries)
